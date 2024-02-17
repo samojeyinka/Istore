b794365731abf11012869cb46acf9cd6acc014a9
samojeyinka <samuelojeyinka@gmail.com>
2024-02-16 13:21:59 -1200

    product show page with similar products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,7 @@ class ProductsController < ApplicationController
 
     def show
         @product = Product.find(params[:id])
+        @similar_products = Product.where(category_id: @product.category_id).where.not(id: @product.id).limit(10)
+          
     end
 end
