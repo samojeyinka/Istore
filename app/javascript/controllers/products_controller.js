d64e854206f4cc50f872e9b418b14949daff0e11
samojeyinka <samuelojeyinka@gmail.com>
2024-02-16 22:33:06 -1200

    implemented adding product ti cart

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -4,9 +4,38 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static values = {size: String, product: Object}
 
-    addToCart(){
-      console.log("producct: ",this.productValue)
+    addToCart() {
+      console.log("product: ", this.productValue)
+      console.log("size: ", this.sizeValue)
+      const cart = localStorage.getItem("cart")
+      if (cart) {
+        const cartArray = JSON.parse(cart)
+        const foundIndex = cartArray.findIndex(item => item.id === this.productValue.id && item.size === this.sizeValue)
+        if (foundIndex >= 0) {
+          cartArray[foundIndex].quantity = parseInt(cartArray[foundIndex].quantity) + 1
+        } else {
+          cartArray.push({
+            id: this.productValue.id,
+            name: this.productValue.name,
+            price: this.productValue.price,
+            size: this.sizeValue,
+            quantity: 1
+          })
+        }
+        localStorage.setItem("cart", JSON.stringify(cartArray))
+      } else {
+        const cartArray = []
+        cartArray.push({
+          id: this.productValue.id,
+          name: this.productValue.name,
+          price: this.productValue.price, 
+          size: this.sizeValue,
+          quantity: 1
+        })
+        localStorage.setItem("cart", JSON.stringify(cartArray))
+      }
     }
+  
 
     selectSize(e) {
       this.sizeValue = e.target.value;
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
 
    resources :products, only: [:show,:index]
    resources :categories, only: [:show]
+
+   get 'cart' => "carts#show"
 end
